b0b3603784449d2941db7e97301853d8296debc5
asefrahman <asef1357@gmail.com>
2016-07-30 15:35:50 -0400

    initial commit

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,16 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForLoop {
+	public void printOut(){
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i<=3; i++){
+			System.out.println("How are you feeling today?");
+			String answer = sc. nextLine();
+
+		}
+		System.out.println("Thank you!");
+	}
+}
+
---
+++ src/loops/TestForLoop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class TestForLoop {
+
+	public static void main(String[] args) {
+		ForLoop test = new ForLoop();
+		test.printOut();
+
+	}
+
+}
